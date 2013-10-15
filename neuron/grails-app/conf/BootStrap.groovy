9ad5101be991c0aec751a40fb71bcc7f3093f5c8
greatkim91 <dongyoul.kim@gmail.com>
2013-10-15 11:43:47 +0900

    add required directories

--- neuron/grails-app/conf/BootStrap.groovy
+++ neuron/grails-app/conf/BootStrap.groovy
@@ -8,19 +8,19 @@ class BootStrap {
 		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
 		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 		
-		newUser 'dykim', '1234', 'DY', adminRole
-		newUser 'dwkim', '1234', 'DW', userRole
-		newUser 'yjkim', '1234', 'YJ', userRole
-		newUser 'aa0', '1234', 'AA0', userRole
-		newUser 'aa1', '1234', 'AA1', userRole
-		newUser 'aa2', '1234', 'AA2', userRole
-		newUser 'aa3', '1234', 'AA3', userRole
-		newUser 'aa4', '1234', 'AA4', userRole
-		newUser 'aa5', '1234', 'AA5', userRole
-		newUser 'aa6', '1234', 'AA6', userRole
-		newUser 'aa7', '1234', 'AA7', userRole
-		newUser 'aa8', '1234', 'AA8', userRole
-		newUser 'aa9', '1234', 'AA9', userRole
+		newUser 'dykim', '1', 'DY', adminRole
+		newUser 'dwkim', '1', 'DW', userRole
+		newUser 'yjkim', '1', 'YJ', userRole
+		newUser 'aa0', '1', 'AA0', userRole
+		newUser 'aa1', '1', 'AA1', userRole
+		newUser 'aa2', '1', 'AA2', userRole
+		newUser 'aa3', '1', 'AA3', userRole
+		newUser 'aa4', '1', 'AA4', userRole
+		newUser 'aa5', '1', 'AA5', userRole
+		newUser 'aa6', '1', 'AA6', userRole
+		newUser 'aa7', '1', 'AA7', userRole
+		newUser 'aa8', '1', 'AA8', userRole
+		newUser 'aa9', '1', 'AA9', userRole
 		
 		assert User.count() == 13
 		assert Role.count() == 2
